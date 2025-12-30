80127fc96a2864cd408d8cc19da2f802d35e8835
Li Bai <lbai@temple.edu>
2025-12-30 15:53:16 -0500

    Update ECE2612 path in updateos.sh

--- updateos.sh
+++ updateos.sh
@@ -1,5 +1,5 @@
 echo 'umask 027'>> ~/.bash_profile
-echo "export ECE2612=/opt">> ~/.bash_profile
+echo "export ECE2612=/tools ">> ~/.bash_profile
 echo 'PATH=$PATH:$ECE2612/bin'>> ~/.bash_profile
 
 mkdir -p ~/.vscode-server/data/Machine
