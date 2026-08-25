3b6ee10f8c87b0dc6a445891a39e205854586cf2
Li Bai <lbai@temple.edu>
2026-08-25 18:44:22 -0400

    Update ECE2612 path in updateos.sh

--- updateos.sh
+++ updateos.sh
@@ -1,5 +1,5 @@
 echo 'umask 027'>> ~/.bash_profile
-echo "export ECE2612=/home/ubuntu/ece2613">> ~/.bash_profile
+echo "export ECE2612=/home/ubuntu/environment/ece2613">> ~/.bash_profile
 echo 'PATH=$PATH:$ECE2612/backdoor'>> ~/.bash_profile
 sed -i 's|^export MODELSIM_ROOTDIR=.*$|export MODELSIM_ROOTDIR=/home/ubuntu/intelFPGA/20.1/modelsim_ase|' ./backdoor/ms_simulate
 
